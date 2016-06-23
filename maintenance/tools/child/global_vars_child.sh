69d0b2b90cc7bc798958b3189b52b5440845bca6
root <root@vagrant-ubuntu-trusty.vagrantup.com>
2016-06-23 09:18:38 +0000

    Updating variables to the latest versions used in Rwanda

--- maintenance/tools/child/global_vars_child.sh
+++ maintenance/tools/child/global_vars_child.sh
@@ -10,12 +10,12 @@ USER_HOME_DIR=/home/$USER
 ROOT_DIR=$USER_HOME_DIR/maintenance
 
 #usually /var/lib/tomcat6/webapps
-TOMCAT_WEBAPP_DIR=/var/lib/tomcat6/webapps
+TOMCAT_WEBAPP_DIR=/var/lib/tomcat7/webapps
 
 #usually /home/openmrs/.OpenMRS/modules
-OPENMRS_MODULES_DIR=/home/openmrs/.OpenMRS/modules
+OPENMRS_MODULES_DIR=/usr/share/tomcat7/.OpenMRS/modules
 
-OPENMRS_RUNTIME_PROPERTIES_FILE=/home/openmrs/.OpenMRS/openmrs-runtime.properties
+OPENMRS_RUNTIME_PROPERTIES_FILE=/usr/share/tomcat7/.OpenMRS/openmrs-runtime.properties
 
 OPENMRS_DATABASE_NAME=openmrs
 
