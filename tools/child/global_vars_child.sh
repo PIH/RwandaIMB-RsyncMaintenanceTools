2906dc5270730f68a8f0570a630ccaca9e70d1f3
James Mbabazi <jmbabazi@pih.org>
2022-07-17 13:15:49 -0400

    updating tomcat user

--- tools/child/global_vars_child.sh
+++ tools/child/global_vars_child.sh
@@ -1,27 +1,15 @@
 #!/bin/bash -e
 #ALL VARIABLE VALUES ARE SET IN THIS FILE
-
 DATE=`exec /bin/date`
-
 USER=openmrs_maintenance_rwanda
-
 USER_HOME_DIR=/home/$USER
-
 ROOT_DIR=$USER_HOME_DIR/maintenance
-
-#usually /var/lib/tomcat6/webapps
-TOMCAT_WEBAPP_DIR=/var/lib/tomcat7/webapps
-
-TOMCAT_USER=tomcat7
-
-TOMCAT_SERVICE=tomcat7
-
+# tomcat9
+TOMCAT_WEBAPP_DIR=/var/lib/tomcat/webapps
+TOMCAT_USER=tomcat
+TOMCAT_SERVICE=tomcat
 #usually /home/openmrs/.OpenMRS/modules
-OPENMRS_MODULES_DIR=/usr/share/tomcat7/.OpenMRS/modules
-
-OPENMRS_RUNTIME_PROPERTIES_FILE=/usr/share/tomcat7/.OpenMRS/openmrs-runtime.properties
-
+OPENMRS_MODULES_DIR=/usr/share/tomcat/.OpenMRS/modules
+OPENMRS_RUNTIME_PROPERTIES_FILE=/usr/share/tomcat/.OpenMRS/openmrs-runtime.properties
 OPENMRS_DATABASE_NAME=openmrs
-
 OPENMRS_WEBAPP_NAME=openmrs
-
